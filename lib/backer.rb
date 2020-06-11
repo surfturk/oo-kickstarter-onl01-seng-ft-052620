982818f7793368bb3a574d65444444ca51fd646b
Berkley Brown <berkleydeane@gmail.com>
2020-06-11 14:15:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,23 +1,23 @@
 class Backer
-  
-  attr_reader :name, :backed_projects
-  
-  
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-    
-  end  
-  
-  
-  def back_project(project)
-    @backed_projects << project
-    project.backed_projects << self
-  end  
-  
-  
-  
-  
-  
-  
-end  
+    attr_reader :name, :backed_projects
+    def initialize(name)
+      @name = name
+      @backed_projects = []
+    end
+    def back_project(project)
+        @backed_projects << project
+        project.backers << self
+    end
+  end
+9:10
+class Project
+    attr_reader :title, :backers
+    def initialize(title)
+      @title = title
+      @backers = []
+    end
+    def add_backer(backer)
+      @backers << backer
+      backer.backed_projects << self
+    end
+  end
